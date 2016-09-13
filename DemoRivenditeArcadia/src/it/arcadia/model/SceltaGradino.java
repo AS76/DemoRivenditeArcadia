66ab39cf5ec0253c738158677e123135f8bd2658
Andrea Sassi <andrea.sassi@gmail.com>
2016-09-13 09:39:53 +0200

    Initial commit
    Change-Id: Idb5080685096b5b6a3f8d011fb92c27c32974d45
    Signed-off-by: andrea <andrea@DESKTOP-FP0FM79.arcadiasrl.local>

---
+++ DemoRivenditeArcadia/src/it/arcadia/model/SceltaGradino.java
@@ -0,0 +1,10 @@
+package it.arcadia.model;
+
+//Dovrei teoricamente utilizzarlo per poter scegliere al posto di 1,2,3 il gradino con il suo nome.
+
+public enum SceltaGradino {
+
+	COSTARETTA,
+	DOPPIAELLE,
+	ELLESINGOLA
+}
